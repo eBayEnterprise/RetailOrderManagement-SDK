268769da3fb6a821a7efde89009b91849d30af0f
Michael A. Smith <msmith3@ebay.com>
2014-09-25 15:34:03 -0400

    Flat Credit Card Request Interface

--- Payload/Payment/IFlatCreditCardAuthRequest.php
+++ Payload/Payment/IFlatCreditCardAuthRequest.php
@@ -179,7 +179,7 @@ interface IFlatCreditCardAuthRequest extends IPayload
      */
     function setBillingLastName($name);
     /**
-     * Billing phone number of the person on the billing address of the credit card
+     * Billing phone number of the person
      *
      * @return string
      */
@@ -190,7 +190,7 @@ interface IFlatCreditCardAuthRequest extends IPayload
      */
     function setBillingPhone($phone);
     /**
-     * The street address and/or suite and building for the billing address of the credit card.
+     * The street address and/or suite and building
      *
      * Newline-delimited string, at most four lines
      * xsd restriction: 1-70 characters per line
@@ -203,7 +203,7 @@ interface IFlatCreditCardAuthRequest extends IPayload
      */
     function setBillingLines($lines);
     /**
-     * Name of the city for the billing address of the credit card.
+     * Name of the city
      *
      * xsd restriction: 1-35 characters
      * @return string
@@ -239,42 +239,222 @@ interface IFlatCreditCardAuthRequest extends IPayload
      * @return self
      */
     function setBillingCountryCode($code);
+    /**
+     * Typically, the string of letters and/or numbers that more closely
+     * specifies the delivery area than just the City component alone,
+     * for example, the Zip Code in the U.S.
+     *
+     * xsd restriction: 1-15 characters
+     * @return string
+     */
     function getBillingPostalCode();
+    /**
+     * @param string $code
+     * @return self
+     */
     function setBillingPostalCode($code);
+    /**
+     * First name of the person on the shipping address of the order
+     *
+     * @return string
+     */
     function getShipToFirstName();
+    /**
+     * @param string $name
+     * @return self
+     */
     function setShipToFirstName($name);
+    /**
+     * Last name of the person on the shipping address of the order
+     *
+     * @return string
+     */
     function getShipToLastName();
+    /**
+     * @param string $name
+     * @return self
+     */
     function setShipToLastName($name);
+    /**
+     * ShipTo phone number of the person
+     *
+     * @return string
+     */
     function getShipToPhone();
+    /**
+     * @param string $phone
+     * @return self
+     */
     function setShipToPhone($phone);
-    function getShipToLine1();
-    function setShipToLine1($line);
-    function getShipToLine2();
-    function setShipToLine2($line);
-    function getShipToLine3();
-    function setShipToLine3($line);
-    function getShipToLine4();
-    function setShipToLine4($line);
+    /**
+     * The street address and/or suite and building
+     *
+     * Newline-delimited string, at most four lines
+     * xsd restriction: 1-70 characters per line
+     * @return string
+     */
+    function getShipToLines();
+    /**
+     * @param string $lines
+     * @return self
+     */
+    function setShipToLines($lines);
+    /**
+     * Name of the city
+     *
+     * xsd restriction: 1-35 characters
+     * @return string
+     */
     function getShipToCity();
+    /**
+     * @param string $city
+     * @return self
+     */
     function setShipToCity($city);
+    /**
+     * Typically a two- or three-digit postal abbreviation for the state or province.
+     * ISO 3166-2 code is recommended, but not required
+     *
+     * xsd restriction: 1-35 characters
+     * @return string
+     */
     function getShipToMainDivision();
+    /**
+     * @param string $div
+     * @return self
+     */
     function setShipToMainDivision($div);
+    /**
+     * Two character country code.
+     *
+     * xsd restriction: 2-40 characters
+     * @return string
+     */
     function getShipToCountryCode();
+    /**
+     * @param string $code
+     * @return self
+     */
     function setShipToCountryCode($code);
+    /**
+     * Typically, the string of letters and/or numbers that more closely
+     * specifies the delivery area than just the City component alone,
+     * for example, the Zip Code in the U.S.
+     *
+     * xsd restriction: 1-15 characters
+     * @return string
+     */
     function getShipToPostalCode();
+    /**
+     * @param string $code
+     * @return self
+     */
     function setShipToPostalCode($code);
+    /**
+     * Indicates that this is an authorization re-submission to correct AVS or CVV2 error.
+     * If set to true, this will process the transaction specifically as an AVS/CVV check.
+     * This is important to set correctly, otherwise the cardholder will have
+     * their credit card authed multiple times for the full payment amount when correcting
+     * AVS/CSC errors.
+     *
+     * @return bool
+     */
     function getIsRequestToCorrectCvvOrAvsError();
+    /**
+     * @param bool $flag
+     * @return self
+     */
     function setIsRequestToCorrectCvvOrAvsError($flag);
+    /**
+     * Verified by VISA (VPAS) e-commerce transactions only.
+     * Verify Enrollment Response from the VERes message,
+     * returned to the POE from the ACS server as a result of a Verify Enrollment Request
+     *
+     * 'Y' - Card eligible for authentication processing
+     * 'N' - Attempted authentication. Card eligible for attempts liability,
+     *       but attempts proof is not available from Issuer.
+     * 'U' - Unable to process or card not eligible for attempts.
+     *
+     * @return string
+     */
     function getAuthenticationAvailable();
+    /**
+     * @param string $token
+     * @return self
+     */
     function setAuthenticationAvailable($token);
+    /**
+     * Verified by VISA (VPAS) / MasterCard Secure Code (UCAF)
+     * Transaction Status: For e-commerce VISA and MasterCard transactions only.
+     * Returned in the PARes message from the ACS server in the “Transaction Status” field.
+     *
+     * 'Y' - Authentication Approved
+     * 'A' - Authentication attempted.
+     * 'U' - Unable to authenticate (due to technical problems or excluded card type).
+     * 'N' - Authentication failed.
+     *
+     * @return string
+     */
     function getAuthenticationStatus();
+    /**
+     * @param string $token
+     * @return self
+     */
     function setAuthenticationStatus($token);
+    /**
+     * E-commerce Verified by VISA and MasterCard SecureCode transactions only.
+     * Data returned in authentication request.
+     * For VISA, this field contains CAVV values in upacked, displayable format (0-9, A-F).
+     * For MasterCard, this field contains the UCAF data in upacked, displayable base-64 format
+     * (A-Z, a-z, 0-9, +, /, -).
+     *
+     * xsd restriction: maxLength 64 characters
+     *
+     * @return string
+     */
     function getCavvUcaf();
+    /**
+     * @param string $data
+     * @return self
+     */
     function setCavvUcaf($data);
+    /**
+     * E-commerce Verified by VISA transactions only.
+     * XID data returned from authentication request
+     * in upacked, displayable format (0-9, AF).
+     *
+     * xsd restriction: maxLength 64 characters
+     *
+     * @return string
+     */
     function getTransactionId();
+    /**
+     * @param string $id
+     * @return self
+     */
     function setTransactionId($id);
+    /**
+     * @return string
+     */
     function getEci();
+    /**
+     * @param string $eci
+     * @return self
+     */
     function setEci($eci);
+    /**
+     * The Issuer Bank ACS (Access Control Server) authenticates the cardholder.
+     * The authentication result is represented by the Payer Authentication Response (PARes)
+     * generated by the Card Issuer ACS.
+     *
+     * xsd restriction: maxLength 10000 characters
+     *
+     * @return string
+     */
     function getPayerAuthenticationResponse();
+    /**
+     * @param string $response
+     * @return self
+     */
     function setPayerAuthenticationResponse($response);
 }
