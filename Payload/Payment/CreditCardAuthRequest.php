0c77c1e222a41dfa5f94bf4738a384c63493132c
Chris Kemmerer <ckemmerer@ebay.com>
2014-09-29 11:48:54 -0400

    Deleted unused interfaces.
    Using flat interfaces for payloads.

--- Payload/Payment/CreditCardAuthRequest.php
+++ Payload/Payment/CreditCardAuthRequest.php
@@ -36,346 +36,205 @@ class CreditCardAuthRequest implements ICreditCardAuthRequest
     protected $isRequestToCorrectCVVOrAVSError;
     protected $secureVerificationData;
 
-    /**
-     * RequestId is used to globally identify a request message and is used
-     * for duplicate request protection.
-     *
-     * @return Checkout\IRequestId
-     */
-    function getRequestId()
+    public function getRequestId()
     {
         return $this->requestId;
     }
 
-    /**
-     * @param Checkout\IRequestId $requestId
-     * @return self
-     */
-    function setRequestId($requestId)
+    public function setRequestId($requestId)
     {
         $this->requestId = $requestId;
         return $this;
     }
 
-    /**
-     * The PaymentContext combines with the tender type in the URI to uniquely identify
-     * a Payment Transaction for an order.
-     *
-     * @return IPaymentContext
-     */
-    function getPaymentContext()
+    public function getPaymentContext()
     {
         return $this->paymentContext;
     }
 
-    /**
-     * @param IPaymentContext $context
-     * @return self
-     */
-    function setPaymentContext(IPaymentContext $context)
+    public function setPaymentContext($context)
     {
         $this->paymentContext = $context;
         return $this;
     }
 
-    /**
-     * Expiration date of the credit card.
-     *
-     * @return \DateTime
-     */
-    function getExpirationDate()
+    public function getExpirationDate()
     {
         return $this->expirationDate;
     }
 
-    /**
-     * @param \DateTime $date
-     * @return self
-     */
-    function setExpirationDate(\DateTime $date)
+    public function setExpirationDate(\DateTime $date)
     {
         $this->expirationDate = $date;
         return $this;
     }
 
-    /**
-     * The CVV2 code found on the back of credit cards.
-     *
-     * @return ICardSecurityCode
-     */
-    function getCardSecurityCode()
+    public function getCardSecurityCode()
     {
         return $this->cardSecurityCode;
     }
 
-    /**
-     * @param ICardSecurityCode $code
-     * @return self
-     */
-    function setCardSecurityCode(ICardSecurityCode $code)
+    public function setCardSecurityCode($code)
     {
         $this->cardSecurityCode = $code;
         return $this;
     }
 
-    /**
-     * Amount to authorize
-     *
-     * @return IAmount
-     */
-    function getAmount()
+    public function getAmount()
     {
         return $this->amount;
     }
 
-    /**
-     * @param IAmount $amount
-     * @return self
-     */
-    function setAmount(IAmount $amount)
+    public function setAmount($amount)
     {
         $this->amount = $amount;
         return $this;
     }
 
-    /**
-     * First name of the person on the Billing Address of the credit card
-     *
-     * @return string
-     */
-    function getBillingFirstName()
+    public function getBillingFirstName()
     {
         return $this->billingFirstName;
     }
 
-    /**
-     * @param string $name
-     * @return self
-     */
-    function setBillingFirstName($name)
+    public function setBillingFirstName($name)
     {
         $this->billingFirstName = $name;
         return $this;
     }
 
-    /**
-     * Last name of the person on the Billing Address of the credit card
-     *
-     * @return string
-     */
-    function getBillingLastName()
+    public function getBillingLastName()
     {
         return $this->billingLastName;
     }
 
-    /**
-     * @param string $name
-     * @return self
-     */
-    function setBillingLastName($name)
+    public function setBillingLastName($name)
     {
         $this->billingLastName = $name;
         return $this;
     }
 
-    /**
-     * Billing phone number of the person on the Billing Address of the credit card
-     *
-     * @return string
-     */
-    function getBillingPhoneNo()
+    public function getBillingPhoneNo()
     {
         return $this->billingPhoneNo;
     }
 
-    /**
-     * @param string $phone
-     * @return self
-     */
-    function setBillingPhoneNo($phone)
+    public function setBillingPhoneNo($phone)
     {
         $this->billingPhoneNo = $phone;
         return $this;
     }
 
-    /**
-     * Billing Address of the credit card.
-     *
-     * @return Checkout\IPhysicalAddress
-     */
-    function getBillingAddress()
+    public function getBillingAddress()
     {
         return $this->billingAddress;
     }
 
-    /**
-     * @param Checkout\IPhysicalAddress $address
-     * @return self
-     */
-    function setBillingAddress(Checkout\IPhysicalAddress $address)
+    public function setBillingAddress($address)
     {
         $this->billingAddress = $address;
         return $this;
     }
 
-    /**
-     * E-mail address of the customer making the purchase.
-     *
-     * @return Checkout\IEmailAddress
-     */
-    function getCustomerEmail()
+    public function getCustomerEmail()
     {
         return $this->customerEmail;
     }
 
-    /**
-     * @param Checkout\IEmailAddress $email
-     * @return self
-     */
-    function setCustomerEmail(Checkout\IEmailAddress $email)
+    public function setCustomerEmail($email)
     {
         $this->customerEmail = $email;
         return $this;
     }
 
-    /**
-     * IP Address of the customer making the purchase.
-     *
-     * @return Checkout\IIPv4Address
-     */
-    function getCustomerIPAddress()
+    public function getCustomerIPAddress()
     {
         return $this->customerIpAddress;
     }
 
-    /**
-     * @param Checkout\IIPv4Address $ip
-     * @return self
-     */
-    function setCustomerIPAddress(Checkout\IIPv4Address $ip)
+    public function setCustomerIPAddress($ip)
     {
         $this->customerIpAddress = $ip;
         return $this;
     }
 
-    /**
-     * First name of the person on the Shipping Address of the Order.
-     *
-     * @return string
-     */
-    function getShipToFirstName()
+    public function getShipToFirstName()
     {
         return $this->shipToFirstName;
     }
 
-    /**
-     * @param string $name
-     * @return self
-     */
-    function setShipToFirstName($name)
+    public function setShipToFirstName($name)
     {
         $this->shipToFirstName = $name;
         return $this;
     }
 
-    /**
-     * Last name of the person on the Shipping Address of the Order.
-     *
-     * @return string
-     */
-    function getShipToLastName()
+    public function getShipToLastName()
     {
         return $this->shipToLastName;
     }
 
-    /**
-     * @param string $name
-     * @return self
-     */
-    function setShipToLastName($name)
+    public function setShipToLastName($name)
     {
         $this->shipToLastName = $name;
         return $this;
     }
 
-    /**
-     * Billing phone number of the person on the Billing Address of the credit card
-     *
-     * @return string
-     */
-    function getShipToPhoneNo()
+    public function getShipToPhoneNo()
     {
         return $this->shipToPhoneNo;
     }
 
-    /**
-     * @param string $phone
-     * @return self
-     */
-    function setShipToPhoneNo($phone)
+    public function setShipToPhoneNo($phone)
     {
         $this->shipToPhoneNo = $phone;
         return $this;
     }
 
-    /**
-     * @return Checkout\IPhysicalAddress
-     */
-    function getShippingAddress()
+    public function getShippingAddress()
     {
         return $this->shippingAddress;
     }
 
-    /**
-     * @param Checkout\IPhysicalAddress $address
-     * @return self
-     */
-    function setShippingAddress(Checkout\IPhysicalAddress $address)
+    public function setShippingAddress($address)
     {
         $this->shippingAddress = $address;
         return $this;
     }
 
-    /**
-     * Indicates that this is an authorization re-submission to correct AVS or CVV2 error.
-     * If set to true, this will process the transaction specifically as an AVS/CVV check.
-     * This is important to set correctly, otherwise the cardholder will have their credit card authed multiple times
-     * for the full payment amount when correcting AVS/CSC errors.
-     *
-     * @return bool
-     */
-    function getIsRequestToCorrectCVVOrAVSError()
+    public function getIsRequestToCorrectCVVOrAVSError()
     {
         return $this->isRequestToCorrectCVVOrAVSError;
     }
 
-    /**
-     * @param bool $flag
-     * @return self
-     */
-    function setIsRequestToCorrectCVVOrAVSError($flag)
+    public function setIsRequestToCorrectCVVOrAVSError($flag)
     {
         $this->isRequestToCorrectCVVOrAVSError = (bool) $flag;
         return $this;
     }
 
-    /**
-     * 3D-Secure(Master Card)/Verified-by-Visa Data
-     *
-     * @return ISecureVerificationData
-     */
-    function getSecureVerificationData()
+    public function getSecureVerificationData()
     {
         return $this->secureVerificationData;
     }
 
-    /**
-     * @param ISecureVerificationData $data
-     * @return self
-     */
-    function setSecureVerificationData(ISecureVerificationData $data)
+    public function setSecureVerificationData($data)
     {
         $this->secureVerificationData = $data;
         return $this;
+    }
+
+    public function validate()
+    {
+
+    }
+
+    public function serialize()
+    {
+
+    }
+
+    public function deserialize($string)
+    {
+
     }
 }
--- Payload/Payment/ICreditCardAuthRequest.php
+++ Payload/Payment/ICreditCardAuthRequest.php
@@ -15,38 +15,70 @@
 
 namespace eBayEnterprise\RetailOrderManagement\Payload\Payment;
 
-use eBayEnterprise\RetailOrderManagement\Payload\Checkout;
 use eBayEnterprise\RetailOrderManagement\Payload\IPayload;
 
+/**
+ * Interface ICreditCardAuthRequest
+ * @package eBayEnterprise\RetailOrderManagement\Payload\Payment
+ *
+ */
 interface ICreditCardAuthRequest extends IPayload
 {
     /**
      * RequestId is used to globally identify a request message and is used
      * for duplicate request protection.
      *
-     * @return Checkout\IRequestId
+     * xsd restrictions: 1-40 characters
+     * @return string
      */
     function getRequestId();
     /**
-     * @param Checkout\IRequestId $requestId
+     * @param string $requestId
      * @return self
      */
     function setRequestId($requestId);
     /**
-     * The PaymentContext combines with the tender type in the URI to uniquely identify
-     * a Payment Transaction for an order.
+     * A unique identifier for the order
+     * The client is responsible for ensuring uniqueness across all transactions the client initiates with this service.
      *
-     * @return IPaymentContext
+     * xsd restrictions: 1-20 characters
+     * @return string
      */
-    function getPaymentContext();
+    function getOrderId();
     /**
-     * @param IPaymentContext $context
+     * @param string $orderId
      * @return self
      */
-    function setPaymentContext(IPaymentContext $context);
+    function setOrderId($orderId);
+    /**
+     * Indicates if the Payment Account Number (PAN) is the actual number, or a representation of the number.
+     *
+     * @return bool true if the PAN is a token, false if it's the actual number
+     */
+    function getPanIsToken();
+    /**
+     * @param bool $isToken
+     * @return self
+     */
+    function setPanIsToken($isToken);
+    /**
+     * Either a tokenized or plain credit card number.
+     *
+     * xsd restrictions: 1-22 characters
+     * @see get/setPanIsToken
+     * @return string
+     */
+    function getCardNumber();
+    /**
+     * @param string $ccNum
+     * @return self
+     */
+    function setCardNumber($ccNum);
     /**
      * Expiration date of the credit card.
      *
+     * xsd note: xsd:gYearMonth
+     * @link http://www.w3.org/TR/xmlschema-2/#gYearMonth
      * @return \DateTime
      */
     function getExpirationDate();
@@ -56,29 +88,74 @@ interface ICreditCardAuthRequest extends IPayload
      */
     function setExpirationDate(\DateTime $date);
     /**
-     * The CVV2 code found on the back of credit cards.
+     * The card security code is the number on the back of the credit card
+     * that is normally required for authorizations.
      *
-     * @return ICardSecurityCode
+     * xsd note: maxLength 4
+     *           pattern (\d{3,4})?
+     * return string
      */
     function getCardSecurityCode();
     /**
-     * @param ICardSecurityCode $code
+     * @param string $cvv
      * @return self
      */
-    function setCardSecurityCode(ICardSecurityCode $code);
+    function setCardSecurityCode($cvv);
     /**
-     * Amount to authorize
+     * The amount to authorize
      *
-     * @return IAmount
+     * xsd note: minimum value 0
+     *           maximum precision 2 decimal places
+     * @return float
      */
     function getAmount();
     /**
-     * @param IAmount $amount
+     * @param float $amount
      * @return self
      */
-    function setAmount(IAmount $amount);
+    function setAmount($amount);
     /**
-     * First name of the person on the Billing Address of the credit card
+     * The 3-character ISO 4217 code that represents
+     * the type of currency being used for a transaction.
+     *
+     * @link http://www.iso.org/iso/home/standards/currency_codes.htm
+     * @return string
+     */
+    function getCurrencyCode();
+    /**
+     * @param string $code
+     * @return self
+     */
+    function setCurrencyCode($code);
+    /**
+     * E-mail address of the customer making the purchase.
+     *
+     * xsd restrictions: 1-70 characters
+     * xsd pattern: ([a-zA-Z0-9_\-])([a-zA-Z0-9_\-\.]*)@(\[((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.){3}|((([a-zA-Z0-9\-]+)\.)+))([a-zA-Z]{2,}|(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\])
+     *
+     * @return string
+     */
+    function getEmail();
+    /**
+     * @param string $email
+     * @return self
+     */
+    function setEmail($email);
+    /**
+     * IP Address of the customer making the purchase.
+     *
+     * xsd pattern: ((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])
+     *
+     * @return string
+     */
+    function getIp();
+    /**
+     * @param string $ip
+     * @return self
+     */
+    function setIp($ip);
+    /**
+     * First name of the person on the billing address of the credit card
      *
      * @return string
      */
@@ -89,7 +166,7 @@ interface ICreditCardAuthRequest extends IPayload
      */
     function setBillingFirstName($name);
     /**
-     * Last name of the person on the Billing Address of the credit card
+     * Last name of the person on the billing address of the credit card
      *
      * @return string
      */
@@ -100,51 +177,82 @@ interface ICreditCardAuthRequest extends IPayload
      */
     function setBillingLastName($name);
     /**
-     * Billing phone number of the person on the Billing Address of the credit card
+     * Billing phone number of the person
      *
      * @return string
      */
-    function getBillingPhoneNo();
+    function getBillingPhone();
     /**
      * @param string $phone
      * @return self
      */
-    function setBillingPhoneNo($phone);
+    function setBillingPhone($phone);
     /**
-     * Billing Address of the credit card.
+     * The street address and/or suite and building
      *
-     * @return Checkout\IPhysicalAddress
+     * Newline-delimited string, at most four lines
+     * xsd restriction: 1-70 characters per line
+     * @return string
      */
-    function getBillingAddress();
+    function getBillingLines();
     /**
-     * @param Checkout\IPhysicalAddress $address
+     * @param string $lines
      * @return self
      */
-    function setBillingAddress(Checkout\IPhysicalAddress $address);
+    function setBillingLines($lines);
     /**
-     * E-mail address of the customer making the purchase.
+     * Name of the city
      *
-     * @return Checkout\IEmailAddress
+     * xsd restriction: 1-35 characters
+     * @return string
      */
-    function getCustomerEmail();
+    function getBillingCity();
     /**
-     * @param Checkout\IEmailAddress $email
+     * @param string $city
      * @return self
      */
-    function setCustomerEmail(Checkout\IEmailAddress $email);
+    function setBillingCity($city);
     /**
-     * IP Address of the customer making the purchase.
+     * Typically a two- or three-digit postal abbreviation for the state or province.
+     * ISO 3166-2 code is recommended, but not required
      *
-     * @return Checkout\IIPv4Address
+     * xsd restriction: 1-35 characters
+     * @return string
      */
-    function getCustomerIPAddress();
+    function getBillingMainDivision();
     /**
-     * @param Checkout\IIPv4Address $ip
+     * @param string $div
      * @return self
      */
-    function setCustomerIPAddress(Checkout\IIPv4Address $ip);
+    function setBillingMainDivision($div);
     /**
-     * First name of the person on the Shipping Address of the Order.
+     * Two character country code.
+     *
+     * xsd restriction: 2-40 characters
+     * @return string
+     */
+    function getBillingCountryCode();
+    /**
+     * @param string $code
+     * @return self
+     */
+    function setBillingCountryCode($code);
+    /**
+     * Typically, the string of letters and/or numbers that more closely
+     * specifies the delivery area than just the City component alone,
+     * for example, the Zip Code in the U.S.
+     *
+     * xsd restriction: 1-15 characters
+     * @return string
+     */
+    function getBillingPostalCode();
+    /**
+     * @param string $code
+     * @return self
+     */
+    function setBillingPostalCode($code);
+    /**
+     * First name of the person on the shipping address of the order
      *
      * @return string
      */
@@ -155,7 +263,7 @@ interface ICreditCardAuthRequest extends IPayload
      */
     function setShipToFirstName($name);
     /**
-     * Last name of the person on the Shipping Address of the Order.
+     * Last name of the person on the shipping address of the order
      *
      * @return string
      */
@@ -166,48 +274,185 @@ interface ICreditCardAuthRequest extends IPayload
      */
     function setShipToLastName($name);
     /**
-     * Billing phone number of the person on the Billing Address of the credit card
+     * ShipTo phone number of the person
      *
      * @return string
      */
-    function getShipToPhoneNo();
+    function getShipToPhone();
     /**
      * @param string $phone
      * @return self
      */
-    function setShipToPhoneNo($phone);
+    function setShipToPhone($phone);
     /**
-     * @return Checkout\IPhysicalAddress
+     * The street address and/or suite and building
+     *
+     * Newline-delimited string, at most four lines
+     * xsd restriction: 1-70 characters per line
+     * @return string
      */
-    function getShippingAddress();
+    function getShipToLines();
     /**
-     * @param Checkout\IPhysicalAddress $address
+     * @param string $lines
      * @return self
      */
-    function setShippingAddress(Checkout\IPhysicalAddress $address);
+    function setShipToLines($lines);
+    /**
+     * Name of the city
+     *
+     * xsd restriction: 1-35 characters
+     * @return string
+     */
+    function getShipToCity();
+    /**
+     * @param string $city
+     * @return self
+     */
+    function setShipToCity($city);
+    /**
+     * Typically a two- or three-digit postal abbreviation for the state or province.
+     * ISO 3166-2 code is recommended, but not required
+     *
+     * xsd restriction: 1-35 characters
+     * @return string
+     */
+    function getShipToMainDivision();
+    /**
+     * @param string $div
+     * @return self
+     */
+    function setShipToMainDivision($div);
+    /**
+     * Two character country code.
+     *
+     * xsd restriction: 2-40 characters
+     * @return string
+     */
+    function getShipToCountryCode();
+    /**
+     * @param string $code
+     * @return self
+     */
+    function setShipToCountryCode($code);
+    /**
+     * Typically, the string of letters and/or numbers that more closely
+     * specifies the delivery area than just the City component alone,
+     * for example, the Zip Code in the U.S.
+     *
+     * xsd restriction: 1-15 characters
+     * @return string
+     */
+    function getShipToPostalCode();
+    /**
+     * @param string $code
+     * @return self
+     */
+    function setShipToPostalCode($code);
     /**
      * Indicates that this is an authorization re-submission to correct AVS or CVV2 error.
      * If set to true, this will process the transaction specifically as an AVS/CVV check.
-     * This is important to set correctly, otherwise the cardholder will have their credit card authed multiple times
-     * for the full payment amount when correcting AVS/CSC errors.
+     * This is important to set correctly, otherwise the cardholder will have
+     * their credit card authed multiple times for the full payment amount when correcting
+     * AVS/CSC errors.
      *
      * @return bool
      */
-    function getIsRequestToCorrectCVVOrAVSError();
+    function getIsRequestToCorrectCvvOrAvsError();
     /**
      * @param bool $flag
      * @return self
      */
-    function setIsRequestToCorrectCVVOrAVSError($flag);
+    function setIsRequestToCorrectCvvOrAvsError($flag);
     /**
-     * 3D-Secure(Master Card)/Verified-by-Visa Data
+     * Verified by VISA (VPAS) e-commerce transactions only.
+     * Verify Enrollment Response from the VERes message,
+     * returned to the POE from the ACS server as a result of a Verify Enrollment Request
      *
-     * @return ISecureVerificationData
+     * 'Y' - Card eligible for authentication processing
+     * 'N' - Attempted authentication. Card eligible for attempts liability,
+     *       but attempts proof is not available from Issuer.
+     * 'U' - Unable to process or card not eligible for attempts.
+     *
+     * @return string
      */
-    function getSecureVerificationData();
+    function getAuthenticationAvailable();
     /**
-     * @param ISecureVerificationData $data
+     * @param string $token
      * @return self
      */
-    function setSecureVerificationData(ISecureVerificationData $data);
+    function setAuthenticationAvailable($token);
+    /**
+     * Verified by VISA (VPAS) / MasterCard Secure Code (UCAF)
+     * Transaction Status: For e-commerce VISA and MasterCard transactions only.
+     * Returned in the PARes message from the ACS server in the “Transaction Status” field.
+     *
+     * 'Y' - Authentication Approved
+     * 'A' - Authentication attempted.
+     * 'U' - Unable to authenticate (due to technical problems or excluded card type).
+     * 'N' - Authentication failed.
+     *
+     * @return string
+     */
+    function getAuthenticationStatus();
+    /**
+     * @param string $token
+     * @return self
+     */
+    function setAuthenticationStatus($token);
+    /**
+     * E-commerce Verified by VISA and MasterCard SecureCode transactions only.
+     * Data returned in authentication request.
+     * For VISA, this field contains CAVV values in upacked, displayable format (0-9, A-F).
+     * For MasterCard, this field contains the UCAF data in upacked, displayable base-64 format
+     * (A-Z, a-z, 0-9, +, /, -).
+     *
+     * xsd restriction: maxLength 64 characters
+     *
+     * @return string
+     */
+    function getCavvUcaf();
+    /**
+     * @param string $data
+     * @return self
+     */
+    function setCavvUcaf($data);
+    /**
+     * E-commerce Verified by VISA transactions only.
+     * XID data returned from authentication request
+     * in upacked, displayable format (0-9, AF).
+     *
+     * xsd restriction: maxLength 64 characters
+     *
+     * @return string
+     */
+    function getTransactionId();
+    /**
+     * @param string $id
+     * @return self
+     */
+    function setTransactionId($id);
+    /**
+     * @return string
+     */
+    function getEci();
+    /**
+     * @param string $eci
+     * @return self
+     */
+    function setEci($eci);
+    /**
+     * The Issuer Bank ACS (Access Control Server) authenticates the cardholder.
+     * The authentication result is represented by the Payer Authentication Response (PARes)
+     * generated by the Card Issuer ACS.
+     *
+     * xsd restriction: maxLength 10000 characters
+     *
+     * @return string
+     */
+    function getPayerAuthenticationResponse();
+    /**
+     * @param string $response
+     * @return self
+     */
+    function setPayerAuthenticationResponse($response);
 }
