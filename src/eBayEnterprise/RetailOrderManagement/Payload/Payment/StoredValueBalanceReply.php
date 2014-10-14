dcb9bc812ff7c7ab6fb74c35021d6844af260973
Michael A. Smith <msmith3@ebay.com>
2014-10-14 13:09:04 -0400

    US15594 Balance Reply and Test

---
+++ src/eBayEnterprise/RetailOrderManagement/Payload/Payment/StoredValueBalanceReply.php
@@ -0,0 +1,277 @@
+<?php
+/**
+ * Copyright (c) 2013-2014 eBay Enterprise, Inc.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @copyright   Copyright (c) 2013-2014 eBay Enterprise, Inc. (http://www.ebayenterprise.com/)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace eBayEnterprise\RetailOrderManagement\Payload\Payment;
+
+use eBayEnterprise\RetailOrderManagement\Payload\IValidatorIterator;
+use eBayEnterprise\RetailOrderManagement\Payload\ISchemaValidator;
+use eBayEnterprise\RetailOrderManagement\Payload\Exception;
+
+/**
+ * Class StoredValueBalanceReply
+ * @package eBayEnterprise\RetailOrderManagement\Payload\Payment
+ */
+class StoredValueBalanceReply implements IStoredValueBalanceReply
+{
+    /** @var string **/
+    protected $cardNumber;
+    /** @var bool **/
+    protected $panIsToken;
+    /** @var float **/
+    protected $balanceAmount;
+    /** @var string **/
+    protected $currencyCode;
+    /** @var string */
+    protected $responseCode;
+    /** @var IValidatorIterator */
+    protected $validators;
+    /** @var ISchemaValidator */
+    protected $schemaValidator;
+    /** @var array XPath expressions to extract required data from the serialized payload (XML) */
+    protected $extractionPaths = [
+        'cardNumber' => 'string(x:PaymentAccountUniqueId)',
+        'balanceAmount' => 'number(x:BalanceAmount)',
+        'currencyCode' => 'string(x:BalanceAmount/@currencyCode)',
+        'responseCode' => 'string(x:ResponseCode)',
+    ];
+    /** @var array property/XPath pairs that take boolean values*/
+    protected $booleanXPaths = [
+        'panIsToken' => 'string(x:PaymentAccountUniqueId/@isToken)'
+    ];
+
+    public function __construct(IValidatorIterator $validators, ISchemaValidator $schemaValidator)
+    {
+        $this->validators = $validators;
+        $this->schemaValidator = $schemaValidator;
+    }
+
+    public function getCardNumber()
+    {
+        return $this->cardNumber;
+    }
+
+    /**
+     * @param string $ccNum
+     * @return self
+     */
+    public function setCardNumber($ccNum)
+    {
+        $this->cardNumber = $this->cleanString($ccNum, 22);
+        return $this;
+    }
+
+    public function getPanIsToken()
+    {
+        return $this->panIsToken;
+    }
+
+    /**
+     * @param bool $isToken
+     * @return self
+     */
+    public function setPanIsToken($isToken)
+    {
+        $this->panIsToken = is_bool($isToken) ? $isToken : null;
+        return $this;
+    }
+
+    public function getBalanceAmount()
+    {
+        return $this->balanceAmount;
+    }
+
+    public function getCurrencyCode()
+    {
+        return $this->currencyCode;
+    }
+
+    public function getResponseCode()
+    {
+        return $this->responseCode;
+    }
+    public function setResponseCode($code)
+    {
+        $this->responseCode = $code;
+    }
+
+    /**
+     * @param float
+     * @return self
+     */
+    public function setBalanceAmount($amount)
+    {
+        $this->balanceAmount = $amount;
+    }
+
+    /**
+     * @param string
+     * @return self
+     */
+    public function setCurrencyCode($code)
+    {
+        $this->currencyCode = $code;
+    }
+
+    /**
+     * Serialize the data into a string of XML.
+     * @throws Exception\InvalidPayload
+     * @return string
+     */
+    public function serialize()
+    {
+        // validate the payload data
+        $this->validate();
+        $xmlString = sprintf(
+            '<%s xmlns="%s">%s</%1$s>',
+            self::ROOT_NODE,
+            self::XML_NS,
+            $this->serializeContents()
+        );
+        $canonicalXml = $this->getPayloadAsDoc($xmlString)->C14N();
+        $this->schemaValidate($canonicalXml);
+        return $canonicalXml;
+    }
+
+    public function deserialize($serializedPayload)
+    {
+        // make sure we received a valid serialization of the payload.
+        $this->schemaValidate($serializedPayload);
+
+        $xpath = $this->getPayloadAsXPath($serializedPayload);
+        foreach ($this->extractionPaths as $property => $path) {
+            $this->$property = $xpath->evaluate($path);
+        }
+        // boolean values have to be handled specially
+        foreach ($this->booleanXPaths as $property => $path) {
+            $value = $xpath->evaluate($path);
+            $this->$property = $this->booleanFromString($value);
+        }
+
+        // payload is only valid of the unserialized data is also valid
+        $this->validate();
+        return $this;
+    }
+
+    public function validate()
+    {
+        foreach ($this->validators as $validator) {
+            $validator->validate($this);
+        }
+        return $this;
+    }
+
+    protected function getSchemaFile()
+    {
+        return __DIR__ . '/schema/' . self::PAYLOAD_SCHEMA;
+    }
+
+    /**
+     * Load the payload XML into a DOMDocument
+     * @param  string $xmlString
+     * @return \DOMDocument
+     */
+    protected function getPayloadAsDoc($xmlString)
+    {
+        $d = new \DOMDocument();
+        $d->loadXML($xmlString);
+        return $d;
+    }
+
+    /**
+     * Load the payload XML into a DOMXPath for querying.
+     * @param string $xmlString
+     * @return DOMXPath
+     */
+    protected function getPayloadAsXPath($xmlString)
+    {
+        $xpath = new \DOMXPath($this->getPayloadAsDoc($xmlString));
+        $xpath->registerNamespace('x', self::XML_NS);
+        return $xpath;
+    }
+
+    /**
+     * Serialize the various parts of the payload into XML strings and
+     * simply concatenate them together.
+     * @return string
+     */
+    protected function serializeContents()
+    {
+        return $this->serializePaymentContext()
+        . $this->serializeResponseCodes()
+        . $this->serializeAmount();
+    }
+
+    /**
+     * Create an XML string representing the PaymentContext nodes
+     * @return string
+     */
+    protected function serializePaymentContext()
+    {
+        return sprintf(
+            '<PaymentAccountUniqueId isToken="%s">%s</PaymentAccountUniqueId>',
+            $this->getPanIsToken() ? 'true' : 'false',
+            $this->getCardNumber()
+        );
+    }
+
+    /**
+     * Create an XML string representing the response code from ROM.
+     * @return string
+     */
+    protected function serializeResponseCodes()
+    {
+        return "<ResponseCode>{$this->getResponseCode()}</ResponseCode>";
+    }
+
+    /**
+     * Create an XML string representing the amount authorized.
+     * @return string
+     */
+    protected function serializeAmount()
+    {
+        return sprintf(
+            '<BalanceAmount currencyCode="%s">%01.2F</BalanceAmount>',
+            $this->getCurrencyCode(),
+            $this->getBalanceAmount()
+        );
+    }
+
+    /**
+     * Validate the serialized data via the schema validator.
+     * @param  string $serializedData
+     * @return self
+     */
+    protected function schemaValidate($serializedData)
+    {
+        $this->schemaValidator->validate($serializedData, $this->getSchemaFile());
+        return $this;
+    }
+
+    /**
+     * Convert "true", "false", "1" or "0" to boolean
+     * Everything else returns null
+     *
+     * @param $string
+     * @return bool|null
+     */
+    protected function booleanFromString($string)
+    {
+        if (!is_string($string)) {
+            return null;
+        }
+        $string = strtolower($string);
+        return (($string === 'true') || ($string === '1'));
+    }
+}
---
+++ tests/eBayEnterprise/RetailOrderManagement/Payload/Payment/StoredValueBalanceReplyTest.php
@@ -0,0 +1,245 @@
+<?php
+/**
+ * Copyright (c) 2013-2014 eBay Enterprise, Inc.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @copyright   Copyright (c) 2013-2014 eBay Enterprise, Inc. (http://www.ebayenterprise.com/)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+namespace eBayEnterprise\RetailOrderManagement\Payload\Payment;
+
+use eBayEnterprise\RetailOrderManagement\Payload;
+
+class StoredValueBalanceReplyTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var Payload\IValidator (stub) */
+    protected $stubValidator;
+    /** @var Payload\IValidatorIterator */
+    protected $validatorIterator;
+    /** @var Payload\ISchemaValidator (stub) */
+    protected $stubSchemaValidator;
+
+    /**
+     * Setup a stub validator and validator iterator for each payload to use
+     */
+    public function setUp()
+    {
+        // use stub to allow validation success/failure to be scripted.
+        $this->stubValidator = $this->getMock('\eBayEnterprise\RetailOrderManagement\Payload\IValidator');
+        $this->validatorIterator = new Payload\ValidatorIterator(array($this->stubValidator));
+        $this->stubSchemaValidator = $this->getMock('\eBayEnterprise\RetailOrderManagement\Payload\ISchemaValidator');
+    }
+
+    /**
+     * Get a new StoredValueBalanceReply payload. Each payload will contain a
+     * ValidatorIterator (self::validatorIterator) containing a single mocked
+     * validator (self::$stubValidator).
+     * @return StoredValueBalanceReply
+     */
+    protected function createNewPayload()
+    {
+        return new StoredValueBalanceReply($this->validatorIterator, $this->stubSchemaValidator);
+    }
+
+    /**
+     * Data provider for invalid payloads
+     * @return array[] Array of arg arrays, each containing a set of payload data suitable for self::buildPayload
+     */
+    public function provideInvalidPayload()
+    {
+        return [
+            [[]], // Empty payload should fail validation.
+            [[
+                'cardNumber' => 'RvS1kwB3eCxzk5lI',
+                'panIsToken' => false,
+                'responseCode' => 'glom', // Invalid response code
+                'amountAuthorized' => 55.99,
+                'currencyCode' => 'USD',
+            ]],
+        ];
+    }
+
+    /**
+     * Data provider for valid payloads
+     * @return array[] Array of arg arrays, each containing a set of payload data suitable for self::buildPayload
+     */
+    public function provideValidPayload()
+    {
+        return [[[
+            'cardNumber' => 'KDVXYXCeFCG8GfH6',
+            'panIsToken' => true,
+            'balanceAmount' => 87.44,
+            'currencyCode' => 'USD',
+            'responseCode' => 'Success',
+        ]]];
+    }
+
+    /**
+     * Create a payload with the provided data injected.
+     * @param  mixed[] $properties key/value pairs of property => value
+     * @return StoredValueBalanceReply
+     */
+    protected function buildPayload($properties)
+    {
+        $payload = $this->createNewPayload();
+        $payloadReflection = new \ReflectionClass($payload);
+        foreach ($properties as $property => $value) {
+            if ($payloadReflection->hasProperty($property)) {
+                $property = $payloadReflection->getProperty($property);
+                $property->setAccessible(true);
+                $property->setValue($payload, $value);
+            }
+        }
+        return $payload;
+    }
+
+    /**
+     * Load the XML from a fixture file and canonicalize it. Returns the
+     * canonical XML string.
+     * @return string
+     */
+    protected function loadXmlTestString()
+    {
+        return file_get_contents(__DIR__ . '/Fixtures/StoredValueBalanceReply.xml');
+    }
+
+    /**
+     * Load some invalid XML from a fixture file and canonicalize it. Returns
+     * the canonical XML string.
+     * @return string
+     */
+    protected function loadXmlInvalidTestString()
+    {
+        return file_get_contents(__DIR__ . '/Fixtures/InvalidStoredValueBalanceReply.xml');
+    }
+
+    /**
+     * Simply ensure that when one validator fails validation, the exception
+     * is thrown - is not validating the actual payload data.
+     * @param array $payloadData
+     * @dataProvider provideInvalidPayload
+     * @expectedException \eBayEnterprise\RetailOrderManagement\Payload\Exception\InvalidPayload
+     */
+    public function testValidateWillFail(array $payloadData)
+    {
+        $payload = $this->buildPayload($payloadData);
+        // script the validator to fail validation
+        $this->stubValidator->expects($this->any())
+            ->method('validate')
+            ->will($this->throwException(new Payload\Exception\InvalidPayload));
+        $payload->validate();
+    }
+
+    /**
+     * Simply ensure that when none of the validators fail, the payload is
+     * considered valid - is not validating actual payload data.
+     * @param array $payloadData
+     * @dataProvider provideValidPayload
+     */
+    public function testValidateWillPass(array $payloadData)
+    {
+        $payload = $this->buildPayload($payloadData);
+        // script the validator to pass validation
+        $this->stubValidator->expects($this->any())
+            ->method('validate')
+            ->will($this->returnSelf());
+        $this->assertSame($payload, $payload->validate());
+    }
+
+    /**
+     * Tests that serialize will perform validation. Should any validator
+     * fail, serialization should also fail.
+     * @param array $payloadData
+     * @dataProvider provideInvalidPayload
+     * @expectedException \eBayEnterprise\RetailOrderManagement\Payload\Exception\InvalidPayload
+     */
+    public function testSerializeWillFailWithInvalidPayload(array $payloadData)
+    {
+        $this->stubValidator->expects($this->any())
+            ->method('validate')
+            ->will($this->throwException(new Payload\Exception\InvalidPayload));
+        $payload = $this->buildPayload($payloadData);
+        $payload->serialize();
+    }
+
+    /**
+     * Test that if a payload should pass validation but still produce an
+     * XSD invalid payload, serialization should fail.
+     * @param array $payloadData
+     * @dataProvider provideInvalidPayload
+     * @expectedException \eBayEnterprise\RetailOrderManagement\Payload\Exception\InvalidPayload
+     */
+    public function testSerializeWillFailWithXsdInvalidPayloadData(array $payloadData)
+    {
+        $this->stubSchemaValidator->expects($this->any())
+            ->method('validate')
+            ->will($this->throwException(new Payload\Exception\InvalidPayload));
+        $payload = $this->buildPayload($payloadData);
+        $payload->serialize();
+    }
+    /**
+     * @param array $payloadData
+     * @dataProvider provideValidPayload
+     */
+    public function testSerializeWillPass(array $payloadData)
+    {
+        $payload = $this->buildPayload($payloadData);
+        $domPayload = new \DOMDocument();
+        $domPayload->preserveWhiteSpace = false;
+        $domPayload->loadXML($payload->serialize());
+        $serializedString = $domPayload->C14N();
+        $domPayload->loadXML($this->loadXmlTestString());
+        $expectedString = $domPayload->C14N();
+
+        $this->assertSame($expectedString, $serializedString);
+    }
+
+    /**
+     * @expectedException \eBayEnterprise\RetailOrderManagement\Payload\Exception\InvalidPayload
+     */
+    public function testDeserializeWillFailSchemaInvalid()
+    {
+        $this->stubSchemaValidator->expects($this->any())
+            ->method('validate')
+            ->will($this->throwException(new Payload\Exception\InvalidPayload));
+        $xml = $this->loadXmlInvalidTestString();
+
+        $newPayload = $this->createNewPayload();
+        $newPayload->deserialize($xml);
+    }
+
+    /**
+     * @expectedException \eBayEnterprise\RetailOrderManagement\Payload\Exception\InvalidPayload
+     */
+    public function testDeserializeWillFailPayloadInvalid()
+    {
+        $this->stubValidator->expects($this->any())
+            ->method('validate')
+            ->will($this->throwException(new Payload\Exception\InvalidPayload));
+        $xml = $this->loadXmlInvalidTestString();
+
+        $newPayload = $this->createNewPayload();
+        $newPayload->deserialize($xml);
+    }
+
+    /**
+     * @param array $payloadData
+     * @dataProvider provideValidPayload
+     */
+    public function testDeserializeWillPass(array $payloadData)
+    {
+        $payload = $this->buildPayload($payloadData);
+        $xml = $this->loadXmlTestString();
+        $newPayload = $this->createNewPayload();
+        $newPayload->deserialize($xml);
+
+        $this->assertEquals($payload, $newPayload);
+    }
+}
